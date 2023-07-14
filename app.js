dd5a5de9c1ce8e7404ff00e85b038a3f8d19fe61
pankaj95 <mohit@avioxtechnologies.com>
2023-07-14 23:26:44 +0530

    asd

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
     res.render('</p>I am your big fan</p>')
 })
 
-let PORT = 8080
+let PORT = 3000
 app.listen(PORT, () => {
-    console.log("server is working")
+    console.log(`server is working${PORT}`)
 })
