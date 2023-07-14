e97bb3e40a3740e339685b0f25e464ab8957211d
pankaj95 <mohit@avioxtechnologies.com>
2023-07-14 23:46:43 +0530

    asd

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require("express")
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.render('</p>I am your big fan</p>')
-})
+app.get('/hello', (req, res) => {
+    res.render('<p>I am your big fan</p>');
+});
 
-let PORT = 3000
+let PORT = 8081
 app.listen(PORT, () => {
     console.log(`server is working${PORT}`)
 })
